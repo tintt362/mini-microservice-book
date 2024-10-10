5324eb4678c37cbfe379dd94c77f3d597ad52671
tintt362@gmail.com <tintt362@gmail.com>
2024-10-10 20:02:13 +0700

    update

--- web-app/src/configurations/configuration.js
+++
@@ -1,9 +0,0 @@
-export const CONFIG = {
-  API_GATEWAY: "http://localhost:8888/api/v1",
-};
-
-export const API = {
-  LOGIN: "/identity/auth/token",
-  MY_INFO: "/profile/users/my-profile",
-  MY_POST: "/post/my-posts",
-};
